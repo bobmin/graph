52a5593b44dc71f8a4c5120f5af6cbde1d56ba59
bobmin <maik@btmx.net>
2016-06-08 19:59:18 +0200

    - add support for junit

---
+++ graph/test/bur/graph/BarGraphTest.java
@@ -0,0 +1,19 @@
+package bur.graph;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Prüft die Klasse {@link BarGraph}.
+ * 
+ * @author maik.boettcher@bur-kg.de
+ *
+ */
+public class BarGraphTest {
+
+	@Test
+	public void testData() {
+		Assert.assertTrue(1 == (2 - 1));
+	}
+
+}
